f62ad0bb8104c5b54f7c077b2e0d65f6fc60035e
AnDNTSE184673 <andntse184673@fpt.edu.vn>
2025-05-21 13:04:34 +0700

    Initial Db setup

--- HealthMate/HealthMate/Program.cs
+++ HealthMate/HealthMate/Program.cs
@@ -1,6 +1,11 @@
+using HealthMateRepository.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<NutritionAppContext>(
+       options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
