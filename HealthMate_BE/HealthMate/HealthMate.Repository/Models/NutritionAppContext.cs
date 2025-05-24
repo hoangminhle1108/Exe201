2de394cc045cfad0322d55c43211323e08394f64
hoangminh11 <147319801+hoangminhle1108@users.noreply.github.com>
2025-05-24 16:32:42 +0700

    initial Prj

---
+++ HealthMate_BE/HealthMate/HealthMate.Repository/Models/NutritionAppContext.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace HealthMate.Repository.Models;
+
+public partial class NutritionAppContext : DbContext
+{
+    public NutritionAppContext()
+    {
+    }
+
+    public NutritionAppContext(DbContextOptions<NutritionAppContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Article> Articles { get; set; }
+
+    public virtual DbSet<HealthMetric> HealthMetrics { get; set; }
+
+    public virtual DbSet<PaymentMethod> PaymentMethods { get; set; }
+
+    public virtual DbSet<PremiumPackage> PremiumPackages { get; set; }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<Transaction> Transactions { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+    //        => optionsBuilder.UseSqlServer("Server=localhost;Database=NutritionApp;Trusted_Connection=True;uid=sa;pwd=12345;TrustServerCertificate=True;");
+    public static string GetConnectionString(string connectionStringName)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        string connectionString = config.GetConnectionString(connectionStringName);
+        return connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(GetConnectionString("DefaultConnection")).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Article>(entity =>
+        {
+            entity.HasKey(e => e.ArticleId).HasName("PK__Article__9C6270E81607FB46");
+
+            entity.ToTable("Article");
+
+            entity.Property(e => e.Author).HasMaxLength(100);
+            entity.Property(e => e.Content).HasColumnType("ntext");
+            entity.Property(e => e.PublishedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Title).HasMaxLength(200);
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<HealthMetric>(entity =>
+        {
+            entity.HasKey(e => e.MetricId).HasName("PK__HealthMe__561056A546619A36");
+
+            entity.ToTable("HealthMetric");
+
+            entity.Property(e => e.BodyFat).HasColumnType("decimal(5, 2)");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Height).HasColumnType("decimal(5, 2)");
+            entity.Property(e => e.Note).HasMaxLength(500);
+            entity.Property(e => e.Weight).HasColumnType("decimal(5, 2)");
+
+            entity.HasOne(d => d.User).WithMany(p => p.HealthMetrics)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__HealthMet__UserI__5070F446");
+        });
+
+        modelBuilder.Entity<PaymentMethod>(entity =>
+        {
+            entity.HasKey(e => e.PaymentMethodId).HasName("PK__PaymentM__DC31C1D32DBB11E3");
+
+            entity.ToTable("PaymentMethod");
+
+            entity.Property(e => e.MethodName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<PremiumPackage>(entity =>
+        {
+            entity.HasKey(e => e.PackageId).HasName("PK__PremiumP__322035CC4B33ECAF");
+
+            entity.ToTable("PremiumPackage");
+
+            entity.Property(e => e.Description).HasMaxLength(500);
+            entity.Property(e => e.PackageName).HasMaxLength(100);
+            entity.Property(e => e.Price).HasColumnType("decimal(10, 2)");
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.RoleId).HasName("PK__Role__8AFACE1AA60D7888");
+
+            entity.ToTable("Role");
+
+            entity.HasIndex(e => e.RoleName, "UQ__Role__8A2B616057AFB272").IsUnique();
+
+            entity.Property(e => e.RoleName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Transaction>(entity =>
+        {
+            entity.HasKey(e => e.TransactionId).HasName("PK__Transact__55433A6BD7BFAED3");
+
+            entity.ToTable("Transaction");
+
+            entity.HasIndex(e => e.TransactionCode, "UQ__Transact__D85E702682DAA545").IsUnique();
+
+            entity.Property(e => e.Amount).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.PurchasedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Status).HasMaxLength(50);
+            entity.Property(e => e.TransactionCode).HasMaxLength(100);
+
+            entity.HasOne(d => d.Package).WithMany(p => p.Transactions)
+                .HasForeignKey(d => d.PackageId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Transacti__Packa__48CFD27E");
+
+            entity.HasOne(d => d.PaymentMethod).WithMany(p => p.Transactions)
+                .HasForeignKey(d => d.PaymentMethodId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Transacti__Payme__49C3F6B7");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Transactions)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Transacti__UserI__47DBAE45");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__User__1788CC4C4C5FF3D0");
+
+            entity.ToTable("User");
+
+            entity.HasIndex(e => e.Email, "UQ__User__A9D10534BCCF0657").IsUnique();
+
+            entity.Property(e => e.AvatarUrl).HasMaxLength(255);
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.FullName).HasMaxLength(100);
+            entity.Property(e => e.IsActive).HasDefaultValue(true);
+            entity.Property(e => e.PasswordHash).HasMaxLength(255);
+            entity.Property(e => e.PremiumExpiry).HasColumnType("datetime");
+            entity.Property(e => e.TermsAcceptedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Role).WithMany(p => p.Users)
+                .HasForeignKey(d => d.RoleId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__User__RoleId__3F466844");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ HealthMate_BE/HealthMate/HealthMate.Repository/Models/Transaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HealthMate.Repository.Models;
+
+public partial class Transaction
+{
+    public int TransactionId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int PackageId { get; set; }
+
+    public int PaymentMethodId { get; set; }
+
+    public string TransactionCode { get; set; } = null!;
+
+    public decimal Amount { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public DateTime PurchasedAt { get; set; }
+
+    public virtual PremiumPackage Package { get; set; } = null!;
+
+    public virtual PaymentMethod PaymentMethod { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
